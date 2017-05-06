37b6c22df3917d8180637d7e6c21deda58b2bf4f
Vitaliy Boyarsky <boyarsky.vitaliy@live.com>
2017-05-07 01:28:47 +0300

    Save validation state in ErrorState

---
+++ api-gateway-shared/src/main/java/com/github/vitalibo/auth/core/ErrorState.java
@@ -0,0 +1,24 @@
+package com.github.vitalibo.auth.core;
+
+import lombok.experimental.Delegate;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ErrorState extends RuntimeException implements Map<String, Collection<String>> {
+
+    @Delegate
+    private final Map<String, Collection<String>> errors = new HashMap<>();
+
+    public boolean hasErrors() {
+        return !errors.isEmpty();
+    }
+
+    public void addError(String field, String message) {
+        Collection<String> collection = errors.computeIfAbsent(field, o -> new ArrayList<>());
+        collection.add(message);
+    }
+
+}
