13b062ae8219757d4fb7151319b142848acb8720
Vitalii Boiarskyi2 <vitalii_boiarskyi2@epam.com>
2017-07-04 20:34:01 +0300

    Improve deployment of compiled lambdas

--- update-stack.sh
+++ update-stack.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ $# -ne 2 ]; then
   echo "Usage: $0 [user-name] [deployment-bucket]"
   echo ''
@@ -12,33 +14,13 @@ fi
 USER=$1
 S3_BUCKET=$2
 STACK_NAME="${USER}-stack"
+BUILD=`date -u +%Y-%m-%dT%H:%M:%SZ`
 
-check_result() {
-  if [ $? -ne 0 ]; then
-     echo "error: deploy failed"
-     exit 1
-  fi
-}
-
-function copy() {
-  SERVICE_NAME=$1
-  aws s3 cp "authorization-service-${SERVICE_NAME}/target/authorization-service-${SERVICE_NAME}-1.0-SNAPSHOT.jar" "s3://${S3_BUCKET}/${USER}/" --profile my
-  check_result
-}
-
-function update_lambda() {
-  aws lambda update-function-code --function-name "${USER}-$1" \
-    --s3-bucket "${S3_BUCKET}" --s3-key "${USER}/authorization-service-$1-1.0-SNAPSHOT.jar" --profile my 2>&1 >/dev/null
-  check_result
-}
-
-copy 'server'
-copy 'basic-authenticator'
-copy 'jwt-authorizer'
+echo 'Create/Update stack initialized'
+for MODULE in 'server' 'basic-authenticator' 'jwt-authorizer'; do
+  aws s3 cp "authorization-service-${MODULE}/target/authorization-service-${MODULE}-1.0-SNAPSHOT.jar" \
+    "s3://${S3_BUCKET}/${USER}/${BUILD}/"
+done
 
 aws cloudformation deploy --template-file 'stack.json' --stack-name ${STACK_NAME} \
-  --parameter-overrides UserName=${USER} DeploymentBucket=${S3_BUCKET} --profile my
-
-update_lambda 'server'
-update_lambda 'basic-authenticator'
-update_lambda 'jwt-authorizer'
+  --parameter-overrides UserName=${USER} DeploymentBucket=${S3_BUCKET} Build=${BUILD}
